e451f0b7ff2291e46cc64a8f74758f1da90ba13b
Peter Zingg <peter@zingg.name>
2009-04-09 15:06:10 -0700

    first commit

---
+++ kent/aa_kentfield_prefs.js
@@ -0,0 +1,20 @@
+pref("app.update.enabled", false);
+pref("browser.search.update", false);
+pref("browser.shell.checkDefaultBrowser", false);
+pref("browser.startup.homepage_override.mstone", "rv:1.8.1.4");
+pref("extensions.update.enabled", false);
+pref("intl.charsetmenu.browser.cache", "ISO-8859-1");
+pref("network.cookie.prefsMigrated", true);
+pref("privacy.item.downloads", false);
+pref("privacy.item.formdata", false);
+pref("privacy.item.history", false);
+pref("privacy.sanitize.promptOnSanitize", false);
+pref("privacy.sanitize.sanitizeOnShutdown", true);
+pref("security.warn_submit_insecure", false);
+
+/* next line sets ff up for "automatic proxy discovery" */
+pref("network.proxy.type", 4);
+
+/*  next 2 lines were how we did it previously */
+/* pref("network.proxy.type", 2); */
+/* pref("network.proxy.autoconfig_url", "http://wpad.kentfieldschools.org/wpad.dat"); */
